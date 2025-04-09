f5e558b8dfcdc2a9a39f64e8c26d98c0b2a6beae
Akira37 <863781751@qq.com>
2025-04-09 20:20:52 +0800

    Update .gitignore, add routes in main.go, and update go.mod

--- main.go
+++ main.go
@@ -1,17 +1,28 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
 
 func main() {
-	// 创建一个默认路由引擎
 	r := gin.Default()
-	// 配置路由
-	r.GET("/", func(c *gin.Context) {
-		// c.JSON：返回JSON格式的数据
-		c.JSON(200, gin.H{
-			"message": "Hello, Hyplus!",
-		})
+
+	r.GET("/news", func(c *gin.Context) {
+		c.String(http.StatusOK, "This is News page!")
 	})
-	// 启动HTTP服务，默认为0.0.0.0:8080
+
+	r.POST("/add", func(c *gin.Context) {
+		c.String(http.StatusOK, "POST请求-主要用于增加数据")
+	})
+
+	r.PUT("/edit", func(c *gin.Context) {
+		c.String(http.StatusOK, "PUT请求-主要用于编辑数据")
+	})
+
+	r.DELETE("/delete", func(c *gin.Context) {
+		c.String(http.StatusOK, "DELETE请求-主要用于删除数据")
+	})
+
 	r.Run()
 }
