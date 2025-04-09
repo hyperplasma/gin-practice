f118168908a20289d1faed2d7a5221028534d9a0
Akira37 <863781751@qq.com>
2025-04-09 19:47:53 +0800

    Update .gitignore, refactor main.go, and update go.mod

--- main.go
+++ main.go
@@ -1,21 +1,17 @@
 package main
 
-import (
-	"fmt"
-)
-
-//TIP <p>To run your code, right-click the code and select <b>Run</b>.</p> <p>Alternatively, click
-// the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.</p>
+import "github.com/gin-gonic/gin"
 
 func main() {
-	//TIP <p>Press <shortcut actionId="ShowIntentionActions"/> when your caret is at the underlined text
-	// to see how GoLand suggests fixing the warning.</p><p>Alternatively, if available, click the lightbulb to view possible fixes.</p>
-	s := "gopher"
-	fmt.Println("Hello and welcome, %s!", s)
-
-	for i := 1; i <= 5; i++ {
-		//TIP <p>To start your debugging session, right-click your code in the editor and select the Debug option.</p> <p>We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-		// for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.</p>
-		fmt.Println("i =", 100/i)
-	}
+	// 创建一个默认路由引擎
+	r := gin.Default()
+	// 配置路由
+	r.GET("/", func(c *gin.Context) {
+		// c.JSON：返回JSON格式的数据
+		c.JSON(200, gin.H{
+			"message": "Hello, Hyplus!",
+		})
+	})
+	// 启动HTTP服务，默认为0.0.0.0:8080
+	r.Run()
 }
