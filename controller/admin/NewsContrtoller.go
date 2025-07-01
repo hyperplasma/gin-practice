297bcc350f74878a5cddcb19d8f09ea828d306f3
Akira37 <863781751@qq.com>
2025-07-01 19:29:56 +0800

    Update .gitignore to include dist and test directories; add test function in NewsController

--- controller/admin/NewsContrtoller.go
+++ controller/admin/NewsContrtoller.go
@@ -1,6 +1,7 @@
 package admin
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
@@ -10,4 +11,9 @@ type NewsController struct {
 
 func (c NewsController) Index(ctx *gin.Context) {
 	ctx.String(http.StatusOK, "新闻首页")
+}
+
+func test() {
+	mp := make(map[string]string)
+	fmt.Println(mp)
 }
